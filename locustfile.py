c1c9d2569d4006f67cd51749c1977d5d6e7c6160
DaGestan39 <artemzaporozec97@gmail.com>
2026-05-15 15:45:13 +0300

    df

--- locustfile.py
+++ locustfile.py
@@ -1,6 +1,6 @@
 
 from locust import HttpUser, task, between 
-import BeatifulSoup
+
 
 class Browsinguser(HttpUser):
     weight = 3
@@ -8,4 +8,10 @@ class Browsinguser(HttpUser):
 
     @task
     def woman_watch(self):
-        self.client.get("/specialists")
+        self.client.post("/v1/ft", json={
+    "mode": "pagination",
+    "paginationParams": {
+        "page": 1,
+        "size": 36
+    }
+})
