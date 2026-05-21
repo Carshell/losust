fb72685a3cc9565ce3026599a90aee00a162ef5a
DaGestan39 <artemzaporozec97@gmail.com>
2026-05-21 18:17:46 +0300

    /spesialist

--- locustfile.py
+++ locustfile.py
@@ -3,15 +3,8 @@ from locust import HttpUser, task, between
 
 
 class Browsinguser(HttpUser):
-    weight = 3
     wait_time = between(1,5)  
 
     @task
     def woman_watch(self):
-        self.client.post("/v1/ft", json={
-    "mode": "pagination",
-    "paginationParams": {
-        "page": 1,
-        "size": 36
-    }
-})
+        self.client.get("/specialists")
